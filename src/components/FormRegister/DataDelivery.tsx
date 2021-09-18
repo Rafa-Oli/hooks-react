933e1ddab6b2e885c78a282766c760811795f175
Rafaela Oliveira <rafaela.albaniza@gmail.com>
2021-09-18 17:09:03 -0300

    feat: working states

--- src/components/FormRegister/DataDelivery.tsx
+++ src/components/FormRegister/DataDelivery.tsx
@@ -1,11 +1,25 @@
 import { TextField, Button } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import { FormRegisterProps } from './form-register-interface';
 
 function DataDelivery({ onSubmit }: FormRegisterProps) {
+  const [cep, setCep] = useState('');
+  const [address, setAddress] = useState('');
+  const [number, setNumber] = useState('');
+  const [state, setState] = useState('');
+  const [city, setCity] = useState('');
   return (
-    <form>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        onSubmit({ cep, address, number, state, city });
+      }}
+    >
       <TextField
+        value={cep}
+        onChange={(event) => {
+          setCep(event.target.value);
+        }}
         variant='outlined'
         margin='normal'
         fullWidth
@@ -14,6 +28,10 @@ function DataDelivery({ onSubmit }: FormRegisterProps) {
         type='number'
       />
       <TextField
+        value={address}
+        onChange={(event) => {
+          setAddress(event.target.value);
+        }}
         variant='outlined'
         margin='normal'
         fullWidth
@@ -22,6 +40,10 @@ function DataDelivery({ onSubmit }: FormRegisterProps) {
         type='text'
       />
       <TextField
+        value={number}
+        onChange={(event) => {
+          setNumber(event.target.value);
+        }}
         variant='outlined'
         margin='normal'
         fullWidth
@@ -30,6 +52,10 @@ function DataDelivery({ onSubmit }: FormRegisterProps) {
         type='number'
       />
       <TextField
+        value={state}
+        onChange={(event) => {
+          setState(event.target.value);
+        }}
         variant='outlined'
         margin='normal'
         fullWidth
@@ -38,6 +64,10 @@ function DataDelivery({ onSubmit }: FormRegisterProps) {
         type='text'
       />
       <TextField
+        value={city}
+        onChange={(event) => {
+          setCity(event.target.value);
+        }}
         variant='outlined'
         margin='normal'
         fullWidth
--- src/components/FormRegister/DataUser.tsx
+++ src/components/FormRegister/DataUser.tsx
@@ -1,16 +1,22 @@
 import { Button, TextField } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import { FormRegisterProps } from './form-register-interface';
 
 function DataUser({ onSubmit }: FormRegisterProps) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        onSubmit();
+        onSubmit({ email, password });
       }}
     >
       <TextField
+        value={email}
+        onChange={(event) => {
+          setEmail(event.target.value);
+        }}
         variant='outlined'
         margin='normal'
         fullWidth
@@ -20,6 +26,10 @@ function DataUser({ onSubmit }: FormRegisterProps) {
         type='email'
       />
       <TextField
+        value={password}
+        onChange={(event) => {
+          setPassword(event.target.value);
+        }}
         variant='outlined'
         margin='normal'
         fullWidth
