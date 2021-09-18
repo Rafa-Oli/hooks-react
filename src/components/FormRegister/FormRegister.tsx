f8bb083bb115f3ee7941b506a3cd8685c9cc3084
Rafaela Oliveira <rafaela.albaniza@gmail.com>
2021-09-18 17:22:47 -0300

    feat: useEffect

--- src/components/FormRegister/FormRegister.tsx
+++ src/components/FormRegister/FormRegister.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DataPersonal from './DataPersonal';
 import DataUser from './DataUser';
 import DataDelivery from './DataDelivery';
@@ -6,12 +6,22 @@ import { FormRegisterProps } from './form-register-interface';
 
 function FormRegister({ onSubmit, validCPF }: FormRegisterProps) {
   const [stepActual, setStepActual] = useState(0);
+  const [dataCollected, setDataCollected] = useState({});
+
+  useEffect(() => {
+    console.log(dataCollected);
+  });
+
   const forms = [
-    <DataUser onSubmit={next} />,
-    <DataPersonal onSubmit={next} validCPF={validCPF} />,
-    <DataDelivery onSubmit={onSubmit} />,
+    <DataUser onSubmit={collectData} />,
+    <DataPersonal onSubmit={collectData} validCPF={validCPF} />,
+    <DataDelivery onSubmit={collectData} />,
   ];
 
+  function collectData(data: any) {
+    setDataCollected({ ...dataCollected, ...data });
+    next();
+  }
   function next() {
     setStepActual(stepActual + 1);
   }
